11f17713ea539ef5dc09386302ab297a1d2a2e56
hhhh2735 <team8b3@gmail.com>
2025-12-18 07:35:20 +0700

    Update balance.service.js

--- services/balance.service.js
+++ services/balance.service.js
@@ -1,1 +1,13 @@
+const supabase = require("../config/supabase");
 
+async function updateBalance(userId, amount) {
+  const { data, error } = await supabase.rpc("update_balance", {
+    uid: userId,
+    delta: amount,
+  });
+
+  if (error) throw error;
+  return data;
+}
+
+module.exports = { updateBalance };
