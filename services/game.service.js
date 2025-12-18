b604c9a78d273b89d1412ce27cc7296914366583
hhhh2735 <team8b3@gmail.com>
2025-12-18 07:34:59 +0700

    Update game.service.js

--- services/game.service.js
+++ services/game.service.js
@@ -1,1 +1,18 @@
+function rollDice() {
+  return [
+    Math.ceil(Math.random() * 6),
+    Math.ceil(Math.random() * 6),
+    Math.ceil(Math.random() * 6),
+  ];
+}
 
+function calcResult(dices) {
+  const sum = dices.reduce((a, b) => a + b, 0);
+  const isTriple = dices[0] === dices[1] && dices[1] === dices[2];
+
+  if (isTriple) return { type: "triple", sum };
+  if (sum >= 4 && sum <= 10) return { type: "xiu", sum };
+  return { type: "tai", sum };
+}
+
+module.exports = { rollDice, calcResult };
